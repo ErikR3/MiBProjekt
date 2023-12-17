b6a2cd7909f2406bfaf1ef51d150accd9550a43b
sarajhansson <sarajosefinahansson@gmail.com>
2023-12-17 19:26:30 +0100

    Delarna till utrusning plus ett alternativ
    Titta genom kommentarerna :)

--- mib/Test1/src/Utrustning.java
+++ mib/Test1/src/Utrustning.java
@@ -26,48 +26,105 @@ public class Utrustning extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        utrusning = new javax.swing.JLabel();
+        visaUtrustning = new javax.swing.JLabel();
+        btnUtrustning = new javax.swing.JButton();
+        regiNyUtrustning = new javax.swing.JLabel();
+        btnRegi = new javax.swing.JButton();
+        TaBortUtrustning = new javax.swing.JLabel();
+        btnRaderaUtrustning = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Utrustning");
+        utrusning.setText("Utrustning");
 
-        jLabel2.setText("Visa min aktiva utrustning");
+        visaUtrustning.setText("Visa min aktiva utrustning");
 
-        jButton1.setText("jButton1");
+        btnUtrustning.setText("Visa");
+        btnUtrustning.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUtrustningActionPerformed(evt);
+            }
+        });
+
+        regiNyUtrustning.setText("Registera ny utrusnung");
+
+        btnRegi.setText("Visa");
+        btnRegi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegiActionPerformed(evt);
+            }
+        });
+
+        TaBortUtrustning.setText("Ta bort all utrusning ur systemet");
+
+        btnRaderaUtrustning.setText("Radera");
+        btnRaderaUtrustning.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRaderaUtrustningActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(visaUtrustning)
+                    .addComponent(utrusning)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(29, 29, 29)
+                        .addGap(6, 6, 6)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(137, 137, 137)
-                        .addComponent(jButton1)))
-                .addContainerGap(199, Short.MAX_VALUE))
+                            .addComponent(regiNyUtrustning)
+                            .addComponent(btnUtrustning)
+                            .addComponent(btnRegi)
+                            .addComponent(TaBortUtrustning)
+                            .addComponent(btnRaderaUtrustning))))
+                .addContainerGap(191, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(14, 14, 14)
-                .addComponent(jLabel1)
+                .addComponent(utrusning)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addGap(29, 29, 29)
-                .addComponent(jButton1)
-                .addContainerGap(184, Short.MAX_VALUE))
+                .addComponent(visaUtrustning)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnUtrustning, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(regiNyUtrustning)
+                .addGap(18, 18, 18)
+                .addComponent(btnRegi)
+                .addGap(18, 18, 18)
+                .addComponent(TaBortUtrustning)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnRaderaUtrustning)
+                .addContainerGap(67, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    //Denna knapp visar aktiv utrustning som agenter har just nu. 
+    private void btnUtrustningActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUtrustningActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_btnUtrustningActionPerformed
+//    Via denna knapp kan man registera ny utrustning.
+//    Man blir slussad till ett formulär som ger användaren möjlighet att
+//    skriva in ny utrustning.  
+    private void btnRegiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegiActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegiActionPerformed
+
+    
+    //Detta är en knapp som kommer ta bort all utrustning ifrån hela systemt
+    private void btnRaderaUtrustningActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRaderaUtrustningActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_btnRaderaUtrustningActionPerformed
 
     /**
      * @param args the command line arguments
@@ -105,8 +162,12 @@ public class Utrustning extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel TaBortUtrustning;
+    private javax.swing.JButton btnRaderaUtrustning;
+    private javax.swing.JButton btnRegi;
+    private javax.swing.JButton btnUtrustning;
+    private javax.swing.JLabel regiNyUtrustning;
+    private javax.swing.JLabel utrusning;
+    private javax.swing.JLabel visaUtrustning;
     // End of variables declaration//GEN-END:variables
 }
